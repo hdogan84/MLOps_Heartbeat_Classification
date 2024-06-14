a5b5f104bb707641c740eeb4252b7e5cb9a901a0
hdogan84 <dgnhkn@gmail.com>
2024-06-14 15:42:03 +0200

    made predict sample endpoint work at the port 8005

--- src/predict_sample/predict_sample.py
+++ src/predict_sample/predict_sample.py
@@ -35,7 +35,7 @@ logging.basicConfig(
         logging.StreamHandler()
     ]
 )
-logger = logging.getLogger("predict")
+logger = logging.getLogger("predict_sample")
 
 app = FastAPI()
 
@@ -43,168 +43,13 @@ class PredictModelRequest(BaseModel):
     model_name: str = "RFC"
     dataset: str = "Mitbih"
     x_sample: List = 187 * [0.1]
-    target: int = 1 # added a dummy target for now. 
-
-class DataModelRequest(BaseModel):
-    dataset: str = "Mitbih"
-    oversample: str = "False"
     
 
 @app.get("/status")
 async def get_status():
     return {"status": "Prediction API is up"}
 
-
-@app.post("/data_simulation")
-async def load_data_and_get_sample(request: DataModelRequest):
-    """ This endpoint loads the data, draws one sample data (row) and passes to predict_sample endpoint
-    The resultant feature vector is converted to List to have convenient Pydantic data type
-    - SMOTE oversampling will be added for simulation of new data ingestion (incoming data)
-    """
-
-    # Modified the Pydantic model to add one sample of feature data.
-    # Should later add to the Pydantic Model the Predictor model, default Random Forest Classification
-    dataset = request.dataset
-    oversample = request.oversample # This can do a SMOTE oversampling simulation
-
-    logger.info(f"Received Data Load request with dataset: {dataset}")
-
-    try:
-
-        dataset_name = dataset + "_test"
-        if dataset_name not in datasets:
-            logger.error(f"Dataset {dataset_name} not found in datasets dictionary")
-            return {"error": f"Dataset {dataset_name} not found in datasets dictionary"}
-
-        data_path = "../data/"
-        download_datasets(data_path)
-        logger.info(f"Datasets downloaded to {data_path}")
-      
-        dataset_path = "../data/heartbeat/"
-        cached_datasets = prepare_datasets(dataset_path)
-        logger.info(f"Datasets prepared from {dataset_path}")
-
-        X_test = cached_datasets[f"X_test_{dataset}"]
-        y_test = cached_datasets[f"y_test_{dataset}"]
-
-        rand_row, rand_target = select_random_row(X_test=X_test, y_test=y_test)
-        logger.info("Random row selected from test data")
-
-        if isinstance(rand_row, pd.DataFrame):
-            #rand_row = rand_row.values.reshape(1, -1)
-            logger.info("Random row is of type Pd.dataframe")
-
-        # Convert to list
-        rand_row = rand_row.values.reshape(1, -1).tolist()[0]
-        
-        logger.info(f"The drawn feature vector X: {rand_row}  ")
-        logger.info(type(rand_row))
-
-        data_dict = {}
-        data_dict["dataset"] = dataset
-        data_dict["X_sample"] = rand_row
-        data_dict["Label"] = int(rand_target)
-
-        logger.info("-------------------------------------------------------------------------------------------------------------")
-        #return data_dict  # Working Up to this point
-
-    except Exception as e:
-        logger.error(f"Error during data simulation: {e}")
-        #return {"Error during Data simulation": str(e)}
-
-    # Send request to predict sample endpoint. 
-    # (Working Python requests implementation. Httpx client can be tried)
-
-    try: 
-        r = requests.post(
-                "http://data-simulation-api:8004/predict_sample_v2",  # Using service name
-                 params={"input_json": json.dumps(data_dict)}, 
-                 timeout = 5
-            )
-        logger.info("Request line completed") 
-        #r.raise_for_status() 
-    except requests.exceptions.HTTPError as errh: 
-        logger.info("HTTP Error") 
-        logger.info(errh.args[0]) 
-    except requests.exceptions.ReadTimeout as errrt: 
-        logger.info("Time out") 
-    except requests.exceptions.ConnectionError as conerr: 
-        logger.info("Connection error") 
-    except requests.exceptions.RequestException as errex: 
-        logger.info("Exception request") 
-
-    # Impement the return dict based on the response from the prediction_endpoint
-    return_dict = r.json()
-    return return_dict
-
     
-
-@app.post("/predict_sample_v2") # Version with Dict input
-async def make_prediction_v2(input_json: str):
-    # Modified the Pydantic model to add one sample of feature data.
-    # Should later add to the Pydantic Model the Label parameter (Y = x.iloc[i,187])
-
-    request = json.loads(input_json)
-
-    logger.info("Entered prediction_sample V2 script")
-
-    model_name = "RFC"  # Use default for now 
-    dataset = request["dataset"]
-    x_sample = request["X_sample"]
-    rand_target = request["Label"] #This is the True Label
-
-    logger.info(f"Values of feature vector :{x_sample}")
-
-    model_name = model_name + "_" + dataset
-    logger.info(f"Received prediction_sample V2 request :{model_name}")
-
-    # Deleting data loading lines below. This function should predict based on one row of data that is 
-    # give as input to the function
-
-    try:
-        ml_model_deployed = load_deployment_model(model_name=model_name)
-        logger.info("ml_Model loaded successfully with our load_deployment_model function")
-
-        dataset_name = model_name.split("_")[-1] + "_test"
-        if dataset_name not in datasets:
-            logger.error(f"Dataset {dataset_name} not found in datasets dictionary")
-            return {"error": f"Dataset {dataset_name} not found in datasets dictionary"}
-
-
-        with mlflow.start_run():
-            logger.info("mlflow.start_run() entered")
-            mlflow.log_param("model_name", model_name)
-            mlflow.log_param("dataset_name", dataset_name)            
-
-            # This is the input feature variable
-            rand_row = np.array(x_sample).reshape(1,-1)
-
-            logger.info("Beginning to predict.") # Rand row debug print:", rand_row # --> Not useful for debugging the entire row... also logger.info is not used like print() syntax-wise!
-            prediction = predict_with_ml_model(ml_model=ml_model_deployed, X=rand_row)
-            logger.info("predictions with ML-Model from deployment made successfully")
-            
-            prediction_result = {
-                "prediction": prediction.tolist() if isinstance(prediction, np.ndarray) else prediction
-            }
-
-            mlflow.log_param("input_data", rand_row.tolist() if isinstance(rand_row, np.ndarray) else rand_row.to_dict())
-            mlflow.log_param("true_value", rand_target.tolist() if isinstance(rand_target, np.ndarray) else rand_target)
-
-            if isinstance(prediction_result["prediction"], list):
-                mlflow.log_param("predicted_value", prediction_result["prediction"][0])
-            else:
-                mlflow.log_param("predicted_value", prediction_result["prediction"])
-
-        logger.info(f"Prediction successful: {prediction_result}")
-        logger.info(f"True value: {rand_target}")
-        logger.info("-------------------------------------------------------------------------------------------------------------")
-        return {"prediction": prediction_result, "true_value": int(rand_target)}
-
-    except Exception as e:
-        logger.error(f"Error during prediction: {e}")
-        return {"Error during prediction": str(e)}
-
-
 @app.post("/predict_sample")
 async def make_prediction(request: PredictModelRequest):
     # Modified the Pydantic model to add one sample of feature data.
@@ -212,32 +57,19 @@ async def make_prediction(request: PredictModelRequest):
     model_name = request.model_name
     dataset = request.dataset
     x_sample = request.x_sample # This corresponds to one row of data, without the label
-    rand_target = request.target
 
     model_name = model_name + "_" + dataset
     logger.info(f"Received prediction_sample request with model: {model_name}")
-
-    # Deleting data loading lines below. This function should predict based on one row of data that is 
-    # give as input to the function
 
     try:
         ml_model_deployed = load_deployment_model(model_name=model_name)
         logger.info("ml_Model loaded successfully with our load_deployment_model function")
 
-        dataset_name = model_name.split("_")[-1] + "_test"
-        if dataset_name not in datasets:
-            logger.error(f"Dataset {dataset_name} not found in datasets dictionary")
-            return {"error": f"Dataset {dataset_name} not found in datasets dictionary"}
-
-    
-        # No need anymore for random selection within this function 
-        # rand row and rand target are input parameters now
-        # rand_row, rand_target = select_random_row(X_test=X_test, y_test=y_test)
 
         with mlflow.start_run():
             logger.info("mlflow.start_run() entered")
             mlflow.log_param("model_name", model_name)
-            mlflow.log_param("dataset_name", dataset_name)            
+            mlflow.log_param("dataset_name", dataset)            
 
             # This is the input feature variable
             rand_row = np.array(x_sample).reshape(1,-1)
@@ -245,23 +77,24 @@ async def make_prediction(request: PredictModelRequest):
             logger.info("Beginning to predict.") # Rand row debug print:", rand_row # --> Not useful for debugging the entire row... also logger.info is not used like print() syntax-wise!
             prediction = predict_with_ml_model(ml_model=ml_model_deployed, X=rand_row)
             logger.info("predictions with ML-Model from deployment made successfully")
-            
+            logger.info(f"Type of prediction is {type(prediction)}")
+
             prediction_result = {
                 "prediction": prediction.tolist() if isinstance(prediction, np.ndarray) else prediction
             }
 
             mlflow.log_param("input_data", rand_row.tolist() if isinstance(rand_row, np.ndarray) else rand_row.to_dict())
-            mlflow.log_param("true_value", rand_target.tolist() if isinstance(rand_target, np.ndarray) else rand_target)
-
+            
             if isinstance(prediction_result["prediction"], list):
                 mlflow.log_param("predicted_value", prediction_result["prediction"][0])
             else:
                 mlflow.log_param("predicted_value", prediction_result["prediction"])
 
         logger.info(f"Prediction successful: {prediction_result}")
-        logger.info(f"True value: {rand_target}")
+
         logger.info("-------------------------------------------------------------------------------------------------------------")
-        return {"prediction": prediction_result, "true_value": int(rand_target)}
+        
+        return prediction_result
 
     except Exception as e:
         logger.error(f"Error during prediction: {e}")
@@ -274,76 +107,6 @@ def load_deployment_model(model_name):
     model = mlflow.sklearn.load_model(model_uri)
     return model
 
-def select_random_row(X_test, y_test):
-    random_index = np.random.randint(0, len(X_test))
-    rand_row = pd.DataFrame(X_test.iloc[random_index] if hasattr(X_test, 'iloc') else X_test[random_index]).T
-    rand_target = y_test.iloc[random_index] if hasattr(y_test, 'iloc') else y_test[random_index]
-    logger.info(f"random index for random row: {random_index}")
-    return rand_row, rand_target
-
-def download_datasets(download_path, dataset_owner="shayanfazeli", dataset_name="heartbeat"):
-    api = KaggleApi()
-    api.authenticate()
-
-    dataset_folder = os.path.join(download_path, dataset_name)
-    if not os.path.exists(dataset_folder):
-        api.dataset_download_files(dataset_owner + "/" + dataset_name, path=dataset_folder, unzip=True)
-        logger.info("Datasets are downloaded and unzipped.")
-    else:
-        missing_files = [] 
-        for file_name in ["mitbih_test.csv", "mitbih_train.csv", "ptbdb_abnormal.csv", "ptbdb_normal.csv"]:  
-            file_path = os.path.join(dataset_folder, file_name)
-            if not os.path.exists(file_path):
-                missing_files.append(file_name)
-
-        if missing_files:
-            api.dataset_download_files(dataset_owner + "/" + dataset_name, path=dataset_folder, unzip=True, force=True)
-            logger.info("Missing data was downloaded and unzipped. All Datasets are now available.")
-        else:
-            logger.info("All Datasets are already available.")
-
-dataset_cache = {}
-
-def prepare_datasets(path_to_dataset):
-    global dataset_cache
-    if path_to_dataset in dataset_cache:
-        logger.info("Using cached datasets")
-        return dataset_cache[path_to_dataset]
-
-    mitbih_test, mitbih_train, ptbdb_abnormal, ptbdb_normal = load_datasets_in_workingspace(path_to_datasets=path_to_dataset)
-    
-    ptbdb_concated = pd.concat([ptbdb_abnormal, ptbdb_normal], ignore_index=True).sample(frac=1, random_state=42)
-    X_ptbdb = ptbdb_concated.iloc[:, :-1]
-    y_ptbdb = ptbdb_concated.iloc[:, -1]
-    X_train_ptbdb, X_test_ptbdb, y_train_ptbdb, y_test_ptbdb = train_test_split(X_ptbdb, y_ptbdb, test_size=0.25, random_state=42)
-    
-    mitbih_concated = pd.concat([mitbih_test, mitbih_train], ignore_index=True).sample(frac=1, random_state=42)
-    X_mitbih = mitbih_concated.iloc[:, :-1]
-    y_mitbih = mitbih_concated.iloc[:, -1]
-    X_train_mitbih, X_test_mitbih, y_train_mitbih, y_test_mitbih = train_test_split(X_mitbih, y_mitbih, test_size=0.25, random_state=42)
-
-    logger.info("All test and train sets successfully prepared.")
-
-    dataset_cache[path_to_dataset] = {
-        "X_train_Ptbdb": X_train_ptbdb,
-        "X_test_Ptbdb": X_test_ptbdb,
-        "y_train_Ptbdb": y_train_ptbdb,
-        "y_test_Ptbdb": y_test_ptbdb,
-        "X_train_Mitbih": X_train_mitbih,
-        "X_test_Mitbih": X_test_mitbih,
-        "y_train_Mitbih": y_train_mitbih,
-        "y_test_Mitbih": y_test_mitbih
-    }
-
-    return dataset_cache[path_to_dataset]
-
-def load_datasets_in_workingspace(path_to_datasets="./heartbeat"):
-    mitbih_test = pd.read_csv(path_to_datasets + "/" + "mitbih_test.csv", header=None)
-    mitbih_train = pd.read_csv(path_to_datasets + "/" + "mitbih_train.csv", header=None)
-    ptbdb_abnormal = pd.read_csv(path_to_datasets + "/" + "ptbdb_abnormal.csv", header=None)
-    ptbdb_normal = pd.read_csv(path_to_datasets + "/" + "ptbdb_normal.csv", header=None)
-    return mitbih_test, mitbih_train, ptbdb_abnormal, ptbdb_normal
-
 def predict_with_ml_model(ml_model, X):
     logger.info("Debug: Entered the predict_with_ml_model function")
     try: 
@@ -354,9 +117,4 @@ def predict_with_ml_model(ml_model, X):
         logger.info(f"Error making predictions in the predict_with_ml_model_function: {e}")
         return None
 
-datasets = {
-    "Mitbih_test": "../data/heartbeat/mitbih_test.csv",
-    "Mitbih_train": "../data/heartbeat/mitbih_train.csv",
-    "Ptbdb_test": "../data/heartbeat/ptbdb_test.csv",
-    "Ptbdb_train": "../data/heartbeat/ptbdb_train.csv"
-}
+
