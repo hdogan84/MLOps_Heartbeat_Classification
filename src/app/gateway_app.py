7593c1882fbea9be7f3bfadf760fca2a2e45fcc5
hdogan84 <dgnhkn@gmail.com>
2024-06-14 09:06:54 +0200

    fixed the missing logging at Gateway app page

--- src/app/gateway_app.py
+++ src/app/gateway_app.py
@@ -15,18 +15,30 @@ from redis.asyncio import Redis
 
 ##### TO-DOs: ######
 
+# Logging setup
+log_file_path = Path("reports/logs/app.log")
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    handlers=[
+        logging.FileHandler(log_file_path, mode='a'),
+        logging.StreamHandler()
+    ]
+)
+logger = logging.getLogger("Gateway")
+
 #this solution works and doesnt get a warning for depreceated use of @app.on_event("startup"), but is not written as example in the official redis page!
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     await create_db_and_tables()
     redis = Redis(host='redis', port=6379, decode_responses=True) #redis might lead to bugging and not closing the containers correctly, therefore a try structure is established.
     await FastAPILimiter.init(redis)
-    logging.info("Created all initiations for the lifespan in async def lifespan")
+    logger.info("Created all initiations for the lifespan in async def lifespan")
     try:
         yield
     finally:
         await redis.close()
-        logging.info("Redis connection closed")
+        logger.info("Redis connection closed")
 
 app = FastAPI(lifespan=lifespan)
 
@@ -64,23 +76,24 @@ async def get_status():
 async def send_data_simulation_request(model_name: str, dataset: str):
     async with httpx.AsyncClient() as client:
         try:
+            logger.info("Does gateway logger work ?")
             response = await client.post(
                 "http://data-simulation-api:8004/data_simulation",  # Using service name
                 json={"model_name": model_name, "dataset": dataset}
             )
-            logging.info(f"Response Json is: {response.json()}")
+            logger.info(f"Response Json is: {response.json()}")
             response.raise_for_status()
-            logging.info(f"Data simulation request successful for model: {model_name}")
+            logger.info(f"Data simulation request successful for model: {model_name}")
         except httpx.HTTPStatusError as exc:
-            logging.error(f"HTTP error occurred: {exc.response.status_code} - {exc.response.text}")
+            logger.error(f"HTTP error occurred: {exc.response.status_code} - {exc.response.text}")
         except httpx.RequestError as exc:
-            logging.error(f"Request error occurred: {exc}")
+            logger.error(f"Request error occurred: {exc}")
         except Exception as exc:
-            logging.error(f"Unexpected error occurred: {exc}")
+            logger.error(f"Unexpected error occurred: {exc}")
 
 @app.post("/data_simulation", dependencies=[Depends(RateLimiter(times=180, seconds=60))]) #according to a max of 180 Heartbeats per second.
 async def call_data_simulation_api(background_tasks: BackgroundTasks, model_name: str = "RFC", dataset: str = "Mitbih"):
-    logging.info(f"Received data simulation & prediction request with model: {model_name} and Dataset {dataset}")
+    logger.info(f"Received data simulation & prediction request with model: {model_name} and Dataset {dataset}")
     background_tasks.add_task(send_data_simulation_request, model_name, dataset)
     return {"message": "Prediction request received, processing in the background."}
 
@@ -92,17 +105,17 @@ async def send_prediction_request(model_name: str, dataset: str):
                 json={"model_name": model_name, "dataset": dataset}
             )
             response.raise_for_status()
-            logging.info(f"Prediction request successful for model: {model_name}")
+            logger.info(f"Prediction request successful for model: {model_name}")
         except httpx.HTTPStatusError as exc:
-            logging.error(f"HTTP error occurred: {exc.response.status_code} - {exc.response.text}")
+            logger.error(f"HTTP error occurred: {exc.response.status_code} - {exc.response.text}")
         except httpx.RequestError as exc:
-            logging.error(f"Request error occurred: {exc}")
+            logger.error(f"Request error occurred: {exc}")
         except Exception as exc:
-            logging.error(f"Unexpected error occurred: {exc}")
+            logger.error(f"Unexpected error occurred: {exc}")
 
 @app.post("/predict_realtime", dependencies=[Depends(RateLimiter(times=180, seconds=60))]) #according to a max of 180 Heartbeats per second.
 async def call_prediction_api(background_tasks: BackgroundTasks, model_name: str = "RFC", dataset: str = "Mitbih"):
-    logging.info(f"Received prediction_realtime request with model: {model_name} and Dataset {dataset}")
+    logger.info(f"Received prediction_realtime request with model: {model_name} and Dataset {dataset}")
     background_tasks.add_task(send_prediction_request, model_name, dataset)
     return {"message": "Prediction request received, processing in the background."}
 
@@ -114,13 +127,13 @@ async def send_training_request(dataset: str, model_name: str): #model params co
                 json={"dataset": dataset, "model_name": model_name, "model_params": {}}
             )
             response.raise_for_status()
-            logging.info(f"Training request successful for model: {model_name} with dataset: {dataset}")
+            logger.info(f"Training request successful for model: {model_name} with dataset: {dataset}")
         except httpx.HTTPStatusError as exc:
-            logging.error(f"HTTP error occurred: {exc.response.status_code} - {exc.response.text}")
+            logger.error(f"HTTP error occurred: {exc.response.status_code} - {exc.response.text}")
         except httpx.RequestError as exc:
-            logging.error(f"Request error occurred: {exc}")
+            logger.error(f"Request error occurred: {exc}")
         except Exception as exc:
-            logging.error(f"Unexpected error occurred: {exc}")
+            logger.error(f"Unexpected error occurred: {exc}")
 
 @app.post("/train", dependencies=[Depends(RateLimiter(times=5, seconds=60))])
 async def call_training_api(background_tasks: BackgroundTasks, dataset: str = "Ptbdb", model_name: str = "RFC"):
@@ -135,13 +148,13 @@ async def send_update_request(model_name: str, dataset: str, metric_name: str):
                 json={"model_name": model_name, "dataset": dataset, "metric_name": metric_name}
             )
             response.raise_for_status()
-            logging.info(f"Successfully updated model {model_name} on dataset {dataset} with metric {metric_name}")
+            logger.info(f"Successfully updated model {model_name} on dataset {dataset} with metric {metric_name}")
         except httpx.HTTPStatusError as exc:
-            logging.error(f"HTTP error occurred: {exc.response.status_code} - {exc.response.text}")
+            logger.error(f"HTTP error occurred: {exc.response.status_code} - {exc.response.text}")
         except httpx.RequestError as exc:
-            logging.error(f"Request error occurred: {exc}")
+            logger.error(f"Request error occurred: {exc}")
         except Exception as exc:
-            logging.error(f"Unexpected error occurred: {exc}")
+            logger.error(f"Unexpected error occurred: {exc}")
 
 @app.post("/update_model", dependencies=[Depends(RateLimiter(times=5, seconds=60))])
 async def call_update_api(background_tasks: BackgroundTasks, model_name: str = "RFC", dataset: str = "Ptbdb", metric_name: str = "accuracy"):
